e5983ae3a2674fa0529f045bf6c6e49e4be4ca30
Sukrut Daphalapurkar <sukrut.dd@gmail.com>
2025-09-28 17:02:53 +0000

    feat: completed initial project setup and added shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts'
+        ]
+    },
+    ...compat.config({
+        extends: [
+            'next/core-web-vitals',
+            'next/typescript',
+            'eslint-config-prettier'
+        ]
+    }),
+    {
+        files: ['**/*.{js,jsx,ts,tsx,mjs}'],
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module'
+            }
+        },
+        plugins: {
+            prettier: prettierPlugin
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'react/no-escape-entities': 'off'
+        }
+    }
 ];
 
 export default eslintConfig;
